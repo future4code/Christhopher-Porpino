132fd467db29643c65baa848ea17dc85af73a816
Christhopher Porpino <cgporpinof@gmail.com>
2019-11-18 16:20:16 -0300

    tentando fazer a função funcionar

---
+++ semana4/aula1-funções/test.js
@@ -0,0 +1,30 @@
+console.log("Future4");
+
+// function elementCounter(array) {
+
+//     for(const item of array) {
+//         console.log(item)
+//     }
+//     return array.length
+// }
+
+// const quantidade = elementCounter([1,2])
+// const valores = elementCounter ()
+// console.log('quantidade: ', quantidade)
+
+
+// EXERCÍCIO 2
+const verificaParidade = function(n) {
+    console.log("Começando a função")
+    if (numero % 2 === 0) {
+        console.log("Numero é par")
+    } else {
+        console.log("Número é ímpar")
+    }
+}
+
+let res = verificaParidade (20)
+console.log (20)
+
+res = verificaParidade(31)
+console.log(res)
