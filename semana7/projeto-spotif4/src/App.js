10aa13059871e557d4f0a2029a78c73c1a6c0ffa
Christhopher Porpino <cgporpinof@gmail.com>
2019-12-13 13:34:21 -0300

    inicio do projeto

---
+++ semana7/projeto-spotif4/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import CreatePlaylist from './components/CreatePlaylist'
+
+
+function App() {
+  return (
+    <div className="App">
+      <CreatePlaylist />
+    </div>
+  );
+}
+
+export default App;
