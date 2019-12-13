42587f07da2ac46c89aa34a7b57748d035603c15
Christhopher Porpino <cgporpinof@gmail.com>
2019-12-13 15:36:28 -0300

    criei parte que mostra todas as playlists

--- semana7/projeto-spotif4/src/components/ViewAllPlaylists/index.js
+++ semana7/projeto-spotif4/src/components/ViewAllPlaylists/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import axios from 'axios';
 
 const baseURL = "https://us-central1-spotif4.cloudfunctions.net/api"
