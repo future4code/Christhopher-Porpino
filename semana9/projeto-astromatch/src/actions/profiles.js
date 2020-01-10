ef72b551f6d05b305ae7590c216e3cda6abf7755
Christhopher Porpino <cgporpinof@gmail.com>
2020-01-10 16:26:56 -0300

    perfil aparece na tela

--- semana9/projeto-astromatch/src/actions/profiles.js
+++ semana9/projeto-astromatch/src/actions/profiles.js
@@ -1,5 +1,18 @@
 import axios from 'axios'
 
+const setProfileToSwipe = (profile) => ({
+	type: "SET_PROFILE_TO_SWIPE",
+	payload: {
+		profile: profile,
+	}
+})
+
 export const clearSwipes = () => async (dispatch) => {
-	await axios.put('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/darvas/clear')
+	await axios.put('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/christhopher/clear')
+}
+
+export const fetchProfileToSwipe = () => async (dispatch) => {
+	const response = await axios.get('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/christhopher/person')
+
+	dispatch(setProfileToSwipe(response.data.profile))
 }
