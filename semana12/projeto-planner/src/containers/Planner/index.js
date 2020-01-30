130fbd4410d6a8f9855da073693e05cc352b6bf3
Christhopher Porpino <cgporpinof@gmail.com>
2020-01-29 23:12:40 -0300

    first commit

---
+++ semana12/projeto-planner/src/containers/Planner/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class Planner extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>teste</p>
+      </div>
+    );
+  }
+}
+
+export default connect()(Planner);
