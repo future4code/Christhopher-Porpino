464ca9c56adcba6a15dea962c4f97d1569e52ed8
Christhopher Porpino <cgporpinof@gmail.com>
2020-01-31 07:47:32 -0300

    começando a fazer as actions

---
+++ semana12/projeto-planner/src/actions/index.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const baseURL = "https://us-central1-missao-newton.cloudfunctions.net/generic/planner-bouman-christhopher"
+
+const getTasksAction = (tasks) => {
+    return {
+        type: "GET_TASKS",
+        payload: {
+            tasks,
+        }
+    }
+};
+
+//Get Tasks
+export const getTasks = () => async (dispatch) => {
+    const headers = { headers: { "Content-Type": "application/json" } }
+    const response = await axios.get(`${baseURL}`, headers)
+
+    console.log(response.data)
+    console.log(response.data.tasks)
+    dispatch(getTasksAction(response.data.tasks))
+}
+
+//Create Task
+export const createTask = (text, day) => async (dispatch) => {
+    const createTaskInformation = {
+        text,
+        day
+    }
+    const headers = { headers: { "Content-Type": "application/json" } }
+
+    try {
+        await axios.post(`${baseURL}`, createTaskInformation, headers)
+        console.log("deu certoo")
+        dispatch(getTasks())
+
+    } catch {
+        window.alert("Erro criação")
+    }
+
+}
+
--- semana12/projeto-planner/src/containers/Planner/index.js
+++ semana12/projeto-planner/src/containers/Planner/index.js
@@ -1,14 +1,83 @@
 import React from "react";
 import { connect } from "react-redux";
+import TextField from '@material-ui/core/TextField';
+import styled from 'styled-components';
+import { getTasks } from '../../actions'
+
+const Week = styled.section`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: blue;
+`
+
+const Day = styled.ul`
+  padding: 15px;
+  background: yellow;
+`
 
 class Planner extends React.Component {
+  componentDidMount() {
+    this.props.getTasks()
+    
+  }
+
   render() {
     return (
       <div>
-        <p>teste</p>
+        <form>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            label="Nome de Usuário"
+            autoComplete="username"
+            autoFocus
+            component="h1"
+            type="text"
+            name="username"
+          // value={this.state.username}
+          // onChange={this.handleChange}
+          />
+        </form>
+        <Week>
+          <Day> Segunda
+            <li>Segundou</li>
+          </Day>
+          <Day>
+            Terça
+            <li>Terçou</li>
+          </Day>
+          <Day> Quarta
+            <li>Quartou</li>
+          </Day>
+          <Day> Quinta
+            <li>Quintou</li>
+          </Day>
+          <Day> Sexta
+            <li>Sextou</li>
+          </Day>
+          <Day> Sábado
+            <li>Sabadou</li>
+          </Day>
+          <Day> Domingo
+            <li>Domingou</li>
+          </Day>
+        </Week>
       </div>
     );
   }
 }
 
-export default connect()(Planner);
+// const mapStateToProps = state => ({
+//   allTasks: state.posts.allTasks,
+//   selectIdTask: state.posts.selectIdTask,
+// })
+
+const mapDispatchToProps = dispatch => ({
+  getTasks: () => dispatch(getTasks()),
+});
+
+export default connect(null, mapDispatchToProps)(Planner);
