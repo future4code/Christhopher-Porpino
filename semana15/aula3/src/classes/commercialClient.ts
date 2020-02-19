29bd55788d90b0534d590899a4bf205e7cb18416
Christhopher Porpino <cgporpinof@gmail.com>
2020-02-19 16:30:48 -0300

    exercicios 1 a 3

---
+++ semana15/aula3/src/classes/commercialClient.ts
@@ -0,0 +1,19 @@
+import { Commerce } from "../commerce";
+import { Client } from "../client";
+
+export class CommercialClient extends Commerce implements Client {
+
+    constructor(
+        name: string,
+        cnpj: string,
+        cep: string,
+        public clientName: string,
+        public clientNumber: number,
+        public consumedEnergy: number
+    ) {
+        super(name, cnpj, cep)
+    }
+    calculateBill(): number {
+        return 0
+    }
+}
