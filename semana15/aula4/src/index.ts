a927dd249279af80cd817e6c51490aa15078d7cb
Christhopher Porpino <cgporpinof@gmail.com>
2020-02-20 22:58:08 -0300

    iniciei

---
+++ semana15/aula4/src/index.ts
@@ -0,0 +1,6 @@
+import { readFileSync, writeFileSync } from 'fs';
+
+// abstract class Employee {
+//     constructor(public name: string, public payment: number) {}
+//     public abstract sayJob(): void;
+// }
